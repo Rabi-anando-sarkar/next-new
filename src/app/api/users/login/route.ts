4de261be8c1e0bbd62f545cb4736fa2fea521d91
Rabi-anando-sarkar <rabianandos2002@gmail.com>
2024-03-30 05:41:49 +0530

    verify login logout

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,60 @@
+import { connectDB } from "@/dbConfig/dbConfig";
+import User from '@/models/userModel'
+import { NextRequest,NextResponse } from 'next/server'
+import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+
+connectDB()
+
+export async function POST(request: NextRequest){
+    try {
+        const reqBody = await request.json()
+        const {email,password} = reqBody
+        console.log(reqBody);
+
+        const user = await User.findOne({email})
+
+        if(!user){
+            return NextResponse.json(
+                {error : "No user registered with this email"},
+                {status: 400}
+            )
+        }
+
+        console.log("User Exists!");
+
+        const validPassword = await bcryptjs.compare(password, user.password)
+
+        if(!validPassword){
+            return NextResponse.json(
+                {error : "Credentials don't match"},
+                {status: 400}
+            )
+        }
+
+        const tokenPayload = {
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+
+        const token = await jwt.sign(tokenPayload, process.env.TOKEN_SECRET!, {expiresIn: '1d'})
+        
+        const response = NextResponse.json({
+            message: "Logged in succesfully",
+            success: true
+        })
+
+        response.cookies.set("token",token, {
+            httpOnly: true
+        })
+
+        return response
+
+    } catch (error:any) {
+        return NextResponse.json(
+            {error: error.message},
+            {status: 500}
+        )
+    }
+}
