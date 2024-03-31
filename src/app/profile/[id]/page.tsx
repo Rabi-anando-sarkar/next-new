1a1933d820247254c0deb094ad6477e740685e1e
Rabi-anando-sarkar <rabianandos2002@gmail.com>
2024-04-01 03:30:00 +0530

    middlewares

---
+++ src/app/profile/[id]/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function page({params}: any) {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <h1>Profile</h1>
+            <hr />
+            <hr />
+            <p className="text-2xl">Profile page 
+            <span className=" p-2 ml-2 rounded bg-orange-500 text-black">{params.id}</span>
+        </p>
+    </div>
+  )
+}
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
+'use client'
 
-function page() {
+import React, { useState } from 'react'
+import axios from 'axios'
+import Link from 'next/link'
+import { toast } from 'react-hot-toast'
+import { useRouter } from 'next/navigation'
+
+export default function ProfilePage() {
+  const router = useRouter()
+  
+  const [data, setData] = useState("nothing")
+  
+  const getUserDetails = async() => {
+    try {
+      const res = await axios.get("/api/users/aboutme")
+      console.log(res.data);
+      setData(res.data.data._id)
+    } catch (error:any) {
+      console.log(error.message)
+      toast.error(error.message)
+    }
+  }
+
+  const logout = async () => {
+    try {
+      await axios.get('/api/users/logout')
+      toast.success("Logout success")
+      router.push("/login")
+    } catch (error:any) {
+      console.log(error.message)
+      toast.error(error.message)
+    }
+  }
+
   return (
-    <div>page</div>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Profile Page</h1>
+      <hr/>
+      <h2>{data === "nothing" ? "Nothing" : <Link
+        href={`/profile/${data}`}
+      >{data}</Link>}</h2>
+      <hr/>
+      <button 
+        onClick={logout}
+        className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"  
+      >Logout</button>
+      <button 
+        onClick={getUserDetails}
+        className="bg-green-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"  
+      >Get User Details</button>
+    </div>
   )
 }
-
-export default page
